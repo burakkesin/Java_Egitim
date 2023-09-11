74c78e1e3ea684cec4db98a88b4a3276c3a073cd
burak <burakkesin@hotmail.com.tr>
2023-09-11 20:07:01 +0300

    ilk commit

---
+++ src/Veri.java
@@ -0,0 +1,8 @@
+public class Veri{
+    public static void main(String[] args) {
+        String[] yazılımDilleri = {"java" , "python","C"};
+        int[] yazılımDiliSeviyeleri = {1,2,3,4};
+
+        System.out.println(yazılımDilleri[0] + "dilindeki seviyem" +yazılımDiliSeviyeleri[4]);
+    }
+}
